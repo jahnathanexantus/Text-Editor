b43384eabc9defccf4d7fc95d9dd765a0c2793ad
jahnathan x <jahnathanexantus@gmail.com>
2022-05-26 15:07:53 -0400

    heroku

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,25 +14,26 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  
+  console.log("this works");
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ id:1,value: content });
+  const request = store.put({ id: 1, value: content });
   const result = await request;
-  console.log('🚀 - data saved to the database', result);
+  console.log('🚀 - data saved to the database', result.value);
 };
 
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  
+  console.log("this is the get");
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
   const request = store.get(1);
   const result = await request;
   console.log('result.value', result);
+  result ? console.log("data from the database ", result.value ): console.log("no data from database");
   return result?.value;
 };
 
